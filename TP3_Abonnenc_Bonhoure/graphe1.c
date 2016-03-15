024d7dcc5fdefda20332d492e4f525fd6c9ee461
Alicia Abonnenc <alicia.abonnenc@hotmail.com>
2016-03-15 16:31:52 +0100

    indep et complet

--- TP3_Abonnenc_Bonhoure/graphe1.c
+++ TP3_Abonnenc_Bonhoure/graphe1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 #define MAX_NOEUDS     100
 
@@ -25,25 +26,59 @@ int nombre_arcs (pnoeud_t p) {
 
 
 int degre_graphe (pnoeud_t p) {
- 
+	
 }
 
 
 int independant (pnoeud_t p) {
   /* Les aretes du graphe n'ont pas de sommet en commun */
-
+	/** On veut qu'il y ait au plus une arète par ligne, et colonne*/
+	unsigned int i,j;
+	int col,lig;
+	col = 1;
+	lig = 1;
+	i = 0; 
+	
+	while (i<nb_noeuds && col && lig){
+		int sommec = 0;
+		int sommel = 0;
+		j = 0;
+		while(j<nb_noeuds && sommec <=1 && sommel <=1){
+			if( arcs [i][j] != INT_MAX) {
+				sommec++;
+				sommel++;
+			}
+			j++;
+		}
+		col = sommec <=1;
+		lig = sommel <=1;
+		i++;
+	}
+	return(col && lig);
 }
 
 
 int complet (pnoeud_t p) {
-  /* Toutes les paires de sommet du graphe sont jointes par un arc */
-  
+	/* Toutes les paires de sommet du graphe sont jointes par un arc */
+	/** ie. aucun arc inexistant*/
+	unsigned int i,j;
+	int fin = 0;
+	i=0;
+	while( i < nb_noeuds && !fin){
+		j = 0;
+		while (j < nb_noeuds && arcs[i][j] != INT_MAX) {
+			j++;
+		}
+		fin = j!=nb_noeuds;
+	}
+	return (i==nb_noeuds && j==nb_noeuds);
 }
 
 
 
 int main (int argc, char **argv) {
 
+	return 0;
 }
 
   
