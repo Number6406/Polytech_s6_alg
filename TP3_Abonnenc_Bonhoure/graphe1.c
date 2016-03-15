a31abc21e4c17338804ad6e51ffe1869b9775355
Gilles B <bonhoure.gilles@gmail.com>
2016-03-15 16:37:42 +0100

    nb arcs et degre graphe

--- TP3_Abonnenc_Bonhoure/graphe1.c
+++ TP3_Abonnenc_Bonhoure/graphe1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 #define MAX_NOEUDS     100
 
@@ -10,13 +11,24 @@ typedef int pnoeud_t ; // indice du numero de noeud
 
 int nb_noeuds ;
 
+void init() {
+	int i, j;
+
+	for(i=0; i<nb_noeuds; i++) {
+		for(j=0; j<nb_noeuds; j++) {
+			arcs[i][j] = INT_MAX;
+		}
+	}
+
+}
+
 int nombre_arcs (pnoeud_t p) {
 	int i, j;
 	int somme = 0;
 
-	for(i=0; i<MAX_NOEUDS; i++) {
-		for(j=0; j<MAX_NOEUDS; j++) {
-			if(G[i][j]) somme++;
+	for(i=0; i<nb_noeuds; i++) {
+		for(j=0; j<nb_noeuds; j++) {
+			if(arcs[i][j] != INT_MAX) somme++;
 		}
 	}
 	
@@ -25,25 +37,49 @@ int nombre_arcs (pnoeud_t p) {
 
 
 int degre_graphe (pnoeud_t p) {
- 
+	int i, j;
+	int max = INT_MIN;
+
+	for(i=0; i<nb_noeuds; i++) {
+		
+		int degreCourrant = 0;
+		for(j=0; j<nb_noeuds; j++) {
+			if(arcs[i][j] != INT_MAX) {degreCourrant++;}
+		}
+		
+		if(degreCourrant > max) {
+			max = degreCourrant;
+		}
+	}
+	
+	return max;
 }
 
 
 int independant (pnoeud_t p) {
   /* Les aretes du graphe n'ont pas de sommet en commun */
-
+  return 0;
 }
 
 
 int complet (pnoeud_t p) {
   /* Toutes les paires de sommet du graphe sont jointes par un arc */
-  
+  return 0;
 }
 
-
-
 int main (int argc, char **argv) {
-
+	nb_noeuds = 10;
+	pnoeud_t p = 1;
+	
+	init();
+	arcs[0][1] = 1;
+	arcs[0][2] = 5;
+	arcs[1][2] = 12;
+	
+	printf("Nombre de noeuds : %d\n", nombre_arcs(p));
+	printf("Degré du graphe : %d\n", degre_graphe(p));
+	
+	return 0;
 }
 
   
